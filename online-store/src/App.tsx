26a206c81736d2c69f700c811bd5a23ca70cd678
mshns <mishanos412@gmail.com>
2022-12-16 05:05:56 +0300

    feat: add header

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container wrapper">
+      <header className="container header">
+        <a className="logo" href="/" title="Online Store">
+          Online<span>Store</span>
         </a>
+        <input
+          className="header_search"
+          type="search"
+          placeholder="Search..."
+        />
+        <ul className="header_cart">
+          <span className="cart_count">Cart: 100</span>
+          <span className="cart_sum">Sum: $100</span>
+        </ul>
       </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
