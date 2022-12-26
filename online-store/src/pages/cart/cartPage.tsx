6c759d31f023eba228b3f2d0dde244d5f128a0f1
mshns <mishanos412@gmail.com>
2022-12-26 07:52:57 +0300

    feat: add cart summary

--- online-store/src/pages/cart/cartPage.tsx
+++ online-store/src/pages/cart/cartPage.tsx
@@ -1,14 +1,36 @@
+import "./cartPage.scss";
+
 const CartPage = () => {
   return (
-    <div>
-      <p>Its our cart page</p>
-      <p>Its our cart page</p>
-      <p>Its our cart page</p>
-      <p>Its our cart page</p>
-      <p>Its our cart page</p>
-      <p>Its our cart page</p>
-      <p>Its our cart page</p>
-    </div>
+    <main className="cart">
+      <section className="cart_list">
+        <div className="cart_header">
+          <h1 className="cart_title">Cart</h1>
+          <div className="cart_pagination"></div>
+        </div>
+      </section>
+      <section className="cart_sum">
+        <h2 className="cart_subtitle">Summary</h2>
+        <div className="cart-sum_field">
+          <span className="field_title">Products</span>
+          <span className="products-count">10</span>
+        </div>
+        <div className="cart-sum_field">
+          <span className="field_title">Total price</span>
+          <span className="total-price">$100</span>
+        </div>
+        <div className="cart-sum_field">
+          <label htmlFor="promo-code">Promo code</label>
+          <input
+            className="cart-sum_input"
+            type="text"
+            placeholder="Enter promo code"
+            id="promo-code"
+          ></input>
+        </div>
+        <button className="cart-sum_button">Buy now</button>
+      </section>
+    </main>
   );
 };
 
