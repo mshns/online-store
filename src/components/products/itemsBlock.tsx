fc9f4157614e46e67ac6edad79acf8348f348c12
mshns <mishanos412@gmail.com>
2023-01-25 08:01:41 +0300

    refactor: change return

--- src/components/products/itemsBlock.tsx
+++ src/components/products/itemsBlock.tsx
@@ -1,23 +1,20 @@
 import Item from "./item";
+
 import { IBlockItem, IProductItem } from "../../types";
 
 const ProductsList = ({ items, tableState }: IBlockItem) => {
-  if (items.length) {
-    return (
-      <div className={`content_products ${tableState ? "" : "list"}`}>
-        {items.map((item: IProductItem) => (
-          <Item item={item} key={item.id} tableState={tableState} />
-        ))}
-      </div>
-    );
-  } else {
-    return (
-      <div className="notice">
-        <h2 className="notice_title">Products not found</h2>
-        <span className="notice_image ">production_quantity_limits</span>
-      </div>
-    );
-  }
+  return !!items.length ? (
+    <div className={`content_products ${tableState ? "" : "list"}`}>
+      {items.map((item: IProductItem) => (
+        <Item item={item} key={item.id} tableState={tableState} />
+      ))}
+    </div>
+  ) : (
+    <div className="notice">
+      <h2 className="notice_title">Products not found</h2>
+      <span className="notice_image ">production_quantity_limits</span>
+    </div>
+  );
 };
 
 export default ProductsList;
