424f54964c36a8cd674b32da614d4d3a3a638d9b
mshns <mishanos412@gmail.com>
2023-03-07 03:50:32 +0300

    fix: change imports in item.tsx and itemsBlock.tsx

--- src/components/products/item.tsx
+++ src/components/products/item.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import "./Item.scss";
+import "./item.scss";
 
 import useCart from "../../hooks/useCart";
 
--- src/components/products/itemsBlock.tsx
+++ src/components/products/itemsBlock.tsx
@@ -1,4 +1,4 @@
-import Item from "./Item";
+import Item from "./item";
 
 import { IBlockItem, IProductItem } from "../../types";
 
